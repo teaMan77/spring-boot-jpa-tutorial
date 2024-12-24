8fc65c7737974dfc878d1c24fa904a1d9dd316ae
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-24 12:39:31 +0530

    Testing the student repository

--- src/test/java/com/example/springboot/jpa/tutorial/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/springboot/jpa/tutorial/repository/StudentRepositoryTest.java
@@ -1,10 +1,39 @@
 package com.example.springboot.jpa.tutorial.repository;
 
+import com.example.springboot.jpa.tutorial.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DataJpaTest
+//@DataJpaTest
+@SpringBootTest
 class StudentRepositoryTest {
 
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    public void saveStudentTest() {
+        Student student = Student.builder()
+                .firstName("teaMan")
+                .lastName("teaMan")
+                .emailId("teaMan@yopmail.com")
+                .guardianName("VG")
+                .guardianEmail("vg@yopmail.com")
+                .guardianMobile("9000090000")
+                .build();
+
+        studentRepository.save(student);
+    }
+
+    @Test
+    public void getAllStudentsTest() {
+        List<Student> students = studentRepository.findAll();
+        System.out.println(students);
+    }
 }
