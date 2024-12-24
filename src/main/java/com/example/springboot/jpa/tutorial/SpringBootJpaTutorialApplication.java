cfa7c9d66aad2f08cb4a91e99439671c7a92a2e1
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-24 11:10:22 +0530

    initial commit

---
+++ src/main/java/com/example/springboot/jpa/tutorial/SpringBootJpaTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jpa.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot/jpa/tutorial/SpringBootJpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot.jpa.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
