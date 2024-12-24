3db1ae7460d707879da4d46c1faf6704357ce180
teaMan77 <180627854+teaMan77@users.noreply.github.com>
2024-12-24 18:02:37 +0530

    fetch type

--- src/main/java/com/example/springboot/jpa/tutorial/entity/CourseMaterial.java
+++ src/main/java/com/example/springboot/jpa/tutorial/entity/CourseMaterial.java
@@ -1,14 +1,11 @@
 package com.example.springboot.jpa.tutorial.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
-@Data
 @Builder
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CourseMaterial {
@@ -18,7 +15,11 @@ public class CourseMaterial {
     private Long courseMaterialId;
     private String url;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    @ToString.Exclude
     @JoinColumn(name = "course_id")
     private Course course;
 }
